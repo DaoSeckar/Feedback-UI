8f046e13190c464f3420ee833b457da4e4098a70
unknown <tonyseckar@gmail.com>
2022-03-31 18:13:00 -0700

    Final build

---
+++ src/data/FeedbackData.js
@@ -0,0 +1,19 @@
+const FeedbackData = [
+    {
+        id: 1,
+        rating: 10,
+        text: 'amazing work 20/10 pog',
+    },
+    {
+        id: 2,
+        rating: 3,
+        text: 'amazing work 20/10 pog',
+    },
+    {
+        id: 3,
+        rating: 4,
+        text: 'amazing work 20/10 pog',
+    }
+]
+
+export default FeedbackData
